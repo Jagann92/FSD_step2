5a2e79aebb56b55293c48575bb5a890c41770a72
Mikhail <ydmypt@gmail.com>
2020-04-20 15:30:41 +0300

    Added Ion range slider

---
+++ pages/UI-Kit/form-elements/form-elements.js
@@ -0,0 +1,12 @@
+$(".js-range-slider").ionRangeSlider({
+	type: "double",
+	min: 1000,
+	max: 15000,
+	from: 5000,
+	to: 10000,
+	step: 1000,
+	grid: false,
+	hide_from_to: true,
+	hide_min_max: true,
+	skin: "round"
+});

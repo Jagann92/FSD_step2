5f449bdf51522742d17ca2a01012099c136b86fe
Mikhail <ydmypt@gmail.com>
2020-07-14 13:29:22 +0300

    Доделал скрипт и разметку для выпадающего списка

--- library/blocks/expandable-list/expandable-list.js
+++ library/blocks/expandable-list/expandable-list.js
@@ -1,46 +1,41 @@
-var expandableLists = document.querySelectorAll('.expandable-list');
-expandableLists.forEach(function toogle(element, index) {
-	element.onclick = function () {
+const expandableLists = document.querySelectorAll('.expandable-list');
+const footerLists = document.querySelectorAll('.expandable-list.footer__title');
+expandableLists.forEach(function (element, index) {
+	element.addEventListener("click", function () {
 		element.classList.toggle("open");
+		expandableLists.forEach(function (el, i) {
+			if ((i != index) && (el.classList.contains('float'))) {
+				el.classList.remove('open');
+			}
+		})
+	});
+});
+document.addEventListener('click', function (e) {
+	expandableLists.forEach(function (el, i) {
+		if ((el != e.target) && !(el.contains(e.target))) {
+			el.classList.remove('open');
+		}
+	});
+});
+function relock() {
+	let size = document.documentElement.clientWidth;
+	if (size <= 550) {
+		footerLists.forEach(function (el) {
+			el.classList.remove('lock');
+			el.classList.remove('open');
+		})
 	}
-	element.addEventListener(blur, function () {
-		console.log('blur');
-		element.classList.remove("open");
-	})
-})
-var footerLists = document.querySelectorAll('.expandable-list.footer__title');
+	else {
+		footerLists.forEach(function (el) {
+			el.classList.add('lock');
+			el.classList.add('open');
+		})
+	}
+}
+window.onresize = function () {
+	relock();
+}
+window.onload = function () {
+	relock();
+}
 
-window.onload = function lock() {
-	console.log('load');
-	let size = document.documentElement.clientWidth;
-	console.log(size);
-	if (size <= 550) {
-		footerLists.forEach(function (el) {
-			el.classList.remove('lock');
-			el.classList.remove('open');
-		})
-	}
-	else {
-		footerLists.forEach(function (el) {
-			el.classList.add('lock');
-			el.classList.add('open');
-		})
-	}
-};
-window.onresize = function lock() {
-	console.log('resize');
-	let size = document.documentElement.clientWidth;
-	console.log(size);
-	if (size <= 550) {
-		footerLists.forEach(function (el) {
-			el.classList.remove('lock');
-			el.classList.remove('open');
-		})
-	}
-	else {
-		footerLists.forEach(function (el) {
-			el.classList.add('lock');
-			el.classList.add('open');
-		})
-	}
-};
